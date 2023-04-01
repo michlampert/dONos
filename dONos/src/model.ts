24a26302dbe1e26cc98715bd7d34f88fc79c46a8
Mikolaj Kawaler <miikoolaj927@gmail.com>
2023-04-01 19:27:39 +0200

    Add achievements descriptions

--- dONos/src/model.ts
+++ dONos/src/model.ts
@@ -2,3 +2,145 @@ export interface Points {
     plus: number,
     minus: number,
 }
+
+export const achievments = [
+    {
+        icon: "ribbon-outline",
+        title: "Donos tygodnia",
+        desc: "Donos wyróżniający się na tle innych złożonych w danym tygodniu. Najbardziej dotkliwy, najzabawniejszy, a może została zgłoszona najtrudniejsza do wykrycia zbrodnia? ",
+    },
+    {
+        icon: "diamond-outline",
+        title: "Senior donosiciel",
+        desc: "Zaawansowany donosiciel, który wykonał już 100 donosów.",
+    },
+    {
+        icon: "person-outline",
+        title: "Donosiciel",
+        desc: "Osoba dla której donosy nie są już niczym nowym. Odblokowany po 10 donosach.",
+    },
+    {
+        icon: "thumbs-up-outline",
+        title: "Junior donosiciel",
+        desc: "Brawo! Wykonałeś już swój pierwszy donos!",
+    },
+    {
+        icon: "time-outline",
+        title: "Pierwszy donos w ciągu dnia.",
+        desc: "Super! Zacząłeś dzień od donosu.",
+    },
+    {
+        icon: "calendar-outline",
+        title: "Tydzień donoszenia",
+        desc: "Wykonałeś donosy każdego dnia w danym tygodniu.",
+    },
+    {
+        icon: "calendar-number-outline",
+        title: "Wytrwały donosiciel",
+        desc: "Twoja wytrwałość jest godna podziwu! Wykonałeś donosy każdego dnia w danym miesiącu.",
+    },
+    {
+        icon: "camera-outline",
+        title: "Fotodonos",
+        desc: "Zamieściłeś w donosie zdjęcie.",
+    },
+    {
+        icon: "people-outline",
+        title: "Współdonos",
+        desc: "Ten sam donos zamieściły co najmniej dwie osoby.",
+    },
+    {
+        icon: "map-outline",
+        title: "Wszystkie lokalizacje",
+        desc: "Twoje donosy dotyczyły wszystkich dostępnych miejsc w obrębie korpomapy: biuro, kuchnia, pokój socjalny, toaleta.",
+    },
+    {
+        icon: "map-outline",
+        title: "Cichacz",
+        desc: "Dbasz o niezakłócanie ciszy w biurze.",
+    },
+    {
+        icon: "flame-outline",
+        title: "Strażak",
+        desc: "Pilnujesz, żeby nikt nie przypalał jedzenia w biurowej kuchence mikrofalowej.",
+    },
+    {
+        icon: "fish-outline",
+        title: "Śledzik",
+        desc: "Pilnujesz, żeby nie odgrzewać we wspólnej mikroweli śmierdzącego jedzenia - w tym ryb.",
+    },    
+    {
+        icon: "fish-outline",
+        title: "Segregator",
+        desc: "Pilnujesz, żeby segregować prawidłowo śmieci",
+    },    
+    {
+        icon: "flower-outline",
+        title: "Czyścioszek",
+        desc: "Pilnujesz, żeby współpracownicy dbali o higienę osobistą, oszczędzając innym przykrych doświadczeń.",
+    },    
+    {
+        icon: "flower-outline",
+        title: "Prohibitor",
+        desc: "Zgłosiłeś picie alkoholu w pracy lub przyjście do niej pijanym.",
+    },    
+    {
+        icon: "finger-print-outline",
+        title: "Bezpiecznik",
+        desc: "Zgłosiłeś niezablokowanie komputera po odejściu od stanowiska pracy lub inne nadużycie prowadzące do narażenia danych firmy.",
+    },    
+    {
+        icon: "trophy-outline",
+        title: "Achievement achievement",
+        desc: "Dostałeś achievement za zebranie 15 achievementów.",
+    },    
+    {
+        icon: "cart-outline",
+        title: "Schowaj kumplowi rzeczy",
+        desc: "Ktoś doniósł, że schowałeś komuś rzeczy.",
+    },    
+    {
+        icon: "nutrition-outline",
+        title: "Obrońca owoców",
+        desc: "Pilnujesz, żeby nikt nie wychodził ponad swój owocowy przydział w owocowy czwartek - zgłosiłeś to naganne zachowanie.",
+    },    
+    {
+        icon: "bed-outline",
+        title: "Śpioszek",
+        desc: "Doniosłeś o spaniu w pracy.",
+    },    
+    {
+        icon: "bed-outline",
+        title: "Top model",
+        desc: "Doniosłeś o nieodpowiednim ubiorze w pracy.",
+    },    
+    {
+        icon: "telescope-outline",
+        title: "Sokole oko",
+        desc: "Zgłosiłeś coś trudnego do zauważenia.",
+    },    
+    {
+        icon: "logo-facebook",
+        title: "Marek Zuckerberg",
+        desc: "Zgłosiłeś siedzenie w pracy na facebooku.",
+        
+    },
+    {
+        icon: "logo-facebook",
+        title: "YouTube destroyer",
+        desc: "Zgłosiłeś oglądanie filmików w czasie pracy.",
+        
+    },
+    {
+        icon: "game-controller-outline",
+        title: "Game over",
+        desc: "Doniosłeś na granie w godzinach pracy.",
+        
+    },
+    {
+        icon: "logo-amazon",
+        title: "Besos",
+        desc: "Zgłosiłeś robienie zakupów w czasie pracy.",
+        
+    },
+]
